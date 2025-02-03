0eadab93d298c71d9126e917dd0335ed33cd1320
Your Name <you@example.com>
2025-02-03 13:40:29 +0100

    opgav1

---
+++ objekt.js
@@ -0,0 +1,19 @@
+// Opret et objekt med fire egenskaber
+const person = {
+    name: "John",
+    age: 30,
+    city: "Copenhagen",
+    occupation: "Developer"
+};
+
+// Udskriv egenskaberne ved hjælp af dot notation
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+console.log(person.occupation);
+
+// Udskriv egenskaberne ved hjælp af bracket notation
+console.log(person["name"]);
+console.log(person["age"]);
+console.log(person["city"]);
+console.log(person["occupation"]);
