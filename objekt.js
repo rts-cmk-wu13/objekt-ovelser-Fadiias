3b2faf95b2e9d6d1059c45f1c6f6173d98ded173
Your Name <you@example.com>
2025-02-03 13:47:52 +0100

    opgav2

--- objekt.js
+++ objekt.js
@@ -1,9 +1,15 @@
-// Opret et objekt med fire egenskaber
 const person = {
     name: "John",
     age: 30,
     city: "Copenhagen",
-    occupation: "Developer"
+    occupation: "Developer",
+    isEmployed: true,
+    skills: ["JavaScript", "HTML", "CSS"],
+    address: {
+        street: "Main St",
+        number: 123,
+        postalCode: "12345"
+    }
 };
 
 // Udskriv egenskaberne ved hjælp af dot notation
@@ -17,3 +23,12 @@ console.log(person["name"]);
 console.log(person["age"]);
 console.log(person["city"]);
 console.log(person["occupation"]);
+// Løkke til at udskrive værdierne af arrayet
+for (let i = 0; i < person.skills.length; i++) {
+    console.log(person.skills[i]);
+}
+
+// Udskriv værdierne fra det nestede objekt
+console.log(person.address.street);
+console.log(person.address.number);
+console.log(person.address.postalCode);
